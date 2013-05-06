7dcf335a72b4f09d8f40d0e0dac5a42fda1536b3
Dan Lazewatsky <dan@lazewatsky.com>
2013-05-06 12:03:42 -0500

    fixed indentation

--- src/rostop_gui/node_info.py
+++ src/rostop_gui/node_info.py
@@ -34,33 +34,33 @@ import psutil
 ID = '/NODEINFO'
 
 class NodeInfo(object):
-	nodes = dict()
-	def get_node_info(self, node_name):
-		node_api = rosnode.get_api_uri(rospy.get_master(), node_name)
-		code, msg, pid = xmlrpclib.ServerProxy(node_api[2]).getPid(ID)
-		if node_name in self.nodes:
-			return self.nodes[node_name]
-		else:
-			try:
-				p = psutil.Process(pid)
-				self.nodes[node_name] = p
-				return p
-			except:
-				return False
+    nodes = dict()
+    def get_node_info(self, node_name):
+        node_api = rosnode.get_api_uri(rospy.get_master(), node_name)
+        code, msg, pid = xmlrpclib.ServerProxy(node_api[2]).getPid(ID)
+        if node_name in self.nodes:
+            return self.nodes[node_name]
+        else:
+            try:
+                p = psutil.Process(pid)
+                self.nodes[node_name] = p
+                return p
+            except:
+                return False
 
 
-	def get_all_node_info(self):
-		infos = []
-		for node_name in rosnode.get_node_names():
-			info = self.get_node_info(node_name)
-			if info is not False: infos.append((node_name, info))
-		return infos
+    def get_all_node_info(self):
+        infos = []
+        for node_name in rosnode.get_node_names():
+            info = self.get_node_info(node_name)
+            if info is not False: infos.append((node_name, info))
+        return infos
 
-	def get_all_node_fields(self, fields):
-		# import pdb; pdb.set_trace()
-		processes = self.get_all_node_info()
-		infos = []
-		for name, p in processes:
-			infos.append(p.as_dict(fields + ['cmdline', 'get_memory_info']))
-			infos[-1]['node_name'] = name
-		return infos
+    def get_all_node_fields(self, fields):
+        # import pdb; pdb.set_trace()
+        processes = self.get_all_node_info()
+        infos = []
+        for name, p in processes:
+            infos.append(p.as_dict(fields + ['cmdline', 'get_memory_info']))
+            infos[-1]['node_name'] = name
+        return infos
